4cfcf8307b1e933153cf5d38c416107fcd166b54
Alex Dao <adao7000@gmail.com>
2014-11-01 05:42:19 -0400

    finished breeder's equation heritability function

--- app/src/main/java/com/lnyapps/geneticsandevolution/fragments/problemsolverfragments/BreederFragment.java
+++ app/src/main/java/com/lnyapps/geneticsandevolution/fragments/problemsolverfragments/BreederFragment.java
@@ -25,4 +25,24 @@ public class BreederFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_hardy_weinberg, container, false);
         return rootView;
     }
+
+    public double calcHeritability(double startingPhenotype, double selectedPhenotype, double responsePhenotype){
+        double output = (startingPhenotype - responsePhenotype)/(startingPhenotype - selectedPhenotype);
+        return output;
+    }
+
+    public double calcStartingPhenotype(double selectedPhenotype, double responsePhenotype, double heritability){
+        double output = (responsePhenotype - selectedPhenotype*heritability)/(1-heritability);
+        return output;
+    }
+
+    public double calcSelectedPhenotype(double startingPhenotype, double responsePhenotype, double heritability){
+        double output = (responsePhenotype-startingPhenotype)/heritability + startingPhenotype;
+        return output;
+    }
+
+    public double calcResponsePhenotype(double startingPhenotype, double selectedPhenotype, double heritability){
+        double output = startingPhenotype - (startingPhenotype-selectedPhenotype)*heritability;
+        return output;
+    }
 }
