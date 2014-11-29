ac6431060882fc9bb35cdc682f33f1f2c4b7abf3
Alex Dao <adao7000@gmail.com>
2014-11-28 21:36:30 -0600

    graphing 2-tab pages

--- app/src/main/java/com/lnyapps/geneticsandevolution/allelefreak/AlleleFreakFragment.java
+++ app/src/main/java/com/lnyapps/geneticsandevolution/allelefreak/AlleleFreakFragment.java
@@ -3,17 +3,25 @@ package com.lnyapps.geneticsandevolution.allelefreak;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Context;
 
 import com.lnyapps.geneticsandevolution.MainActivity;
 import com.lnyapps.geneticsandevolution.R;
+import com.lnyapps.geneticsandevolution.problemgenerator.MultipleProblemGeneratorFragment;
+import com.lnyapps.geneticsandevolution.problemgenerator.SingleProblemGeneratorFragment;
 
 /**
  * Created by Alex on 11/24/2014.
  */
 public class AlleleFreakFragment extends Fragment {
+
+    ViewPager mViewPager;
+    FragmentPagerAdapter mFragmentPagerAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,5 +34,40 @@ public class AlleleFreakFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(getResources().getString(R.string.allele_freak));
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        Context mContext;
+
+        public SectionsPagerAdapter(Context context, android.support.v4.app.FragmentManager fragmentManager) {
+            super(fragmentManager);
+            mContext = context;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        //TODO: Change the fragments below in getItem
+        //Look up graphing API and implement in new classes (need to create the two fragments)
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            if (position == 0) {
+                return new SingleProblemGeneratorFragment();
+            } else {
+                return new MultipleProblemGeneratorFragment();
+            }
+        }
+
+        @Override
+        public String getPageTitle(int position) {
+            if (position == 0) {
+                return getString(R.string.allele_graph_page);
+            } else {
+                return getString(R.string.allele_parameters_page);
+            }
+        }
     }
 }
