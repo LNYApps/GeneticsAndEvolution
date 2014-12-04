87db5a2c28b170ef51c4ee14c18f6f962e39fd09
Alex Dao <adao7000@gmail.com>
2014-12-03 23:01:56 -0500

    inflated allele freak options menu need to add in the pop-up dialog box

--- app/src/main/java/com/lnyapps/geneticsandevolution/allelefreak/AlleleGraphFragment.java
+++ app/src/main/java/com/lnyapps/geneticsandevolution/allelefreak/AlleleGraphFragment.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -31,6 +34,20 @@ public class AlleleGraphFragment extends Fragment {
         //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         setArguments(args);
     }*/
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_allelefreak, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.allele_freak_guide) {
+            //TODO: insert pop-up dialog
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
