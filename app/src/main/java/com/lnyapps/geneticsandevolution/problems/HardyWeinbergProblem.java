189c7131d987d5020be706bc925f207f48f2ee1e
Jonathan Tseng <jonathanttseng@gmail.com>
2014-12-16 02:14:54 -0500

    Hardy Weinberg random value bug fix

--- app/src/main/java/com/lnyapps/geneticsandevolution/problems/HardyWeinbergProblem.java
+++ app/src/main/java/com/lnyapps/geneticsandevolution/problems/HardyWeinbergProblem.java
@@ -30,15 +30,15 @@ public class HardyWeinbergProblem extends GenEvolProblem {
 
     public double[] solve() {
         double totalCount = mAAcount + mAacount + maacount;
-        double observedAA = mAAcount/totalCount;
-        double observedAa = mAacount/totalCount;
-        double observedaa = maacount/totalCount;
-        double freqA = observedAA + (0.5*observedAa);
+        double observedAA = mAAcount / totalCount;
+        double observedAa = mAacount / totalCount;
+        double observedaa = maacount / totalCount;
+        double freqA = observedAA + (0.5 * observedAa);
         double freqa = 1 - freqA;
-        double expectedAA = freqA*freqA;
-        double expectedAa = 2*freqA*freqa;
-        double expectedaa = freqa*freqa;
-        double f = (expectedAa - observedAa)/expectedAa;
+        double expectedAA = freqA * freqA;
+        double expectedAa = 2 * freqA * freqa;
+        double expectedaa = freqa * freqa;
+        double f = (expectedAa - observedAa) / expectedAa;
         double[] output = new double[7];
         output[0] = observedAA;
         output[1] = observedAa;
@@ -53,9 +53,18 @@ public class HardyWeinbergProblem extends GenEvolProblem {
 
     @Override
     public void randomValues() {
-        mAAcount = (int) (Math.random() * 3000);
-        mAacount = (int) (Math.random() * 3000);
-        maacount = (int) (Math.random() * 3000);
+        if (Math.random() > 0.5) {
+            double first = (Math.random() * 99 + 1) / 100.0;
+            double second = 1 - first;
+            int popSize = (int) (Math.random() * 9901) + 100;
+            mAAcount = (int) (popSize * Math.pow(first, 2));
+            mAacount = (int) (popSize * 2 * first * second);
+            maacount = (int) (popSize * Math.pow(second, 2));
+        } else {
+            mAAcount = (int) (Math.random() * 3000) + 1;
+            mAacount = (int) (Math.random() * 3000) + 1;
+            maacount = (int) (Math.random() * 3000) + 1;
+        }
     }
 
     @Override
@@ -80,7 +89,7 @@ public class HardyWeinbergProblem extends GenEvolProblem {
     @Override
     public String nonEmptyGivenString() {
         return "AA count: " + mAAcount +
-               "\nAa count: " + mAacount +
-               "\naa count: " + maacount;
+                "\nAa count: " + mAacount +
+                "\naa count: " + maacount;
     }
 }
